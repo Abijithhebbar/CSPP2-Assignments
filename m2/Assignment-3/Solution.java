a7d91de65ea2866f8821254cb9553f0875de39b9
Abijithhebbar <abijith.hebbar@msitprogram.net>
2018-08-29 22:24:36 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

---
+++ m2/Assignment-3/Solution.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+/**
+*code to return the powers.
+*/
+public final class Solution {
+    /**
+    *default constructor.
+    */
+    private Solution() {
+
+    }
+    /**
+    *Do not modify this main function.
+    *@param args string
+    */
+    public static void main(final String[] args) {
+        /**
+        *main method
+        */
+        Scanner in = new Scanner(System.in);
+        long base = in.nextInt();
+        long exponent = in.nextInt();
+        long result = power(base, exponent);
+        System.out.println(result);
+    }
+    /**
+    *Need to write the power function and print the output.
+    *@param base long
+    *@param exponent long
+    *@return power
+    */
+    public static long power(final long base, final long exponent) {
+        /**
+        *sub method
+        */
+        if (exponent == 0) {
+            return 1;
+        } else {
+            return base * power(base, exponent - 1);
+        }
+    }
+}

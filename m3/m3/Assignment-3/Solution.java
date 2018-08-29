cca0431fb7b06631a9664b097936dae101150927
Abijithhebbar <abijith.hebbar@msitprogram.net>
2018-08-29 17:18:35 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- m3/m3/Assignment-3/Solution.java
+++ m3/m3/Assignment-3/Solution.java
@@ -1,24 +1,26 @@
 
 import java.util.Scanner;
 /*
-	Do not modify this main function.
-	*/
+    Do not modify this main function.
+    */
 public class Solution {
 
 
     public static void main(String[] args) {
+        /* main function
+        */
 
-        Scanner s = new Scanner(System.in);      
+        Scanner s = new Scanner(System.in);    
         int n1 = s.nextInt();
         int n2 = s.nextInt();
         gcd(n1, n2);
         System.out.println(gcd(n1, n2));
     }
     /*
-	Need to write the gcd function and print the output.
-	*/
+    Need to write the gcd function and print the output.
+    */
     public static int gcd(final int n1, final int n2)
-    /* main function
+    /* gcd function
     */
     {
          if  (n2 == 0)

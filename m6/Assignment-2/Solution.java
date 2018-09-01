966f3276d314464f3164812dc2c9bdadd4eff92e
Abijithhebbar <abijith.hebbar@msitprogram.net>
2018-09-01 10:33:20 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- m6/Assignment-2/Solution.java
+++ m6/Assignment-2/Solution.java
@@ -33,25 +33,25 @@ final class Solution {
                 a[i][j] = 100;
                     }
             else if (a[i][j] > 100 && a[i][j] <= 150) {
-                a[i][j] = 150;
+                a[i][j] = 100;
                 }
             else if (a[i][j] > 150 && a[i][j] <= 200) {
                 a[i][j] = 200;
                 }
             else if (a[i][j] > 200 && a[i][j] <= 250) {
-                a[i][j] = 250;
+                a[i][j] = 200;
                 }
             else if (a[i][j] > 250 && a[i][j] <= 300) {
                 a[i][j] = 300;
             }
             else if (a[i][j] > 300 && a[i][j] <= 350) {
-                a[i][j] = 350;
+                a[i][j] = 300;
                 }
             else if (a[i][j] > 350 && a[i][j] <= 400) {
                 a[i][j] = 400;
                 }
             else if (a[i][j] > 400 && a[i][j] <= 450) {
-                a[i][j] = 450;
+                a[i][j] = 400;
                 }
             else if (a[i][j] > 450 && a[i][j] <= 500) {
                 a[i][j] = 500;

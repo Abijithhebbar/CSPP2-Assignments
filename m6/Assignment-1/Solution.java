cbcb140b2c0519f6acd22bb8066b554a415ea535
Abijithhebbar <abijith.hebbar@msitprogram.net>
2018-09-01 09:48:59 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- m6/Assignment-1/Solution.java
+++ m6/Assignment-1/Solution.java
@@ -25,7 +25,7 @@ public final class Solution {
                 count = 0;
                 for (int j = 2; j < i; j++) {
                     if (i % j == 0) {
-                        count = count +1;
+                        count = count + 1;
                     }
                 }
                 if (count > 0) {

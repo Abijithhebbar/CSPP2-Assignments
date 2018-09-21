0297766db933787024ef119e58e66e437c753570
Abijithhebbar <abijith.hebbar@msitprogram.net>
2018-09-21 11:45:20 +0530

    "check_style_score -> 1 of 3 passed. style score: 0/1 "

--- m23/Assignment-1/Solution.java
+++ m23/Assignment-1/Solution.java
@@ -1,69 +1,80 @@
 import java.util.*;
 import java.io.*;
 class Solution {
+
 	public static void main(String[] args) {
+
 		try {
 			Scanner sc = new Scanner(System.in);
 			String input = sc.nextLine();
 			File inpfile = new File(input);
 			File[] filearr = inpfile.listFiles();
 			System.out.print("\t"+"\t");
-			sc.close();
-			for (File file : filearr) {
-				System.out.print(file.getName()+"\t");
-			}
-			System.out.println();
+            for (File file : filearr){
+            	System.out.print(file.getName()+"\t");
+            }
+            System.out.println();
 			for (int i = 0; i < filearr.length; i++) {
 				System.out.print(filearr[i].getName()+"\t");
 				for (int j = 0; j < filearr.length; j++) {
 					Bagofwords bag = new Bagofwords();
-					Scanner in = new Scanner(System.in);
+
+					Scanner inp = new Scanner(filearr[i]);
 					HashMap<String, Integer> map = new HashMap<>();
 					String[] array;
-					while (in.hasNext()) {
-						String a = in.useDelimiter("\\A").next();
-						array = a.toLowerCase().split(" ");
-						map = Countfrequency(array);
-						int countKeys = map.keySet().size();
-					}
-					in.close();
-					Scanner inp = new Scanner(filearr[j]);
-					HashMap<String, Integer> newmap = new HashMap<>();
-					String[] array1;
 					while (inp.hasNext()) {
-						String b = in.useDelimiter("\\A").next();
-						array1 = b.toLowerCase().split(" ");
-						newmap = Countfrequency(array1);
+						String a = inp.useDelimiter("\\A").next();
+						array = a.toLowerCase().split(" ");
+						map = CountFrequency(array);
+						int  keyscount = map.keySet().size();
+
 					}
 					inp.close();
+					Scanner inp1 = new Scanner(filearr[j]);
+					HashMap<String, Integer> newmap = new HashMap<>();
+					String[] array1;
+					while (inp1.hasNext()) {
+						String b = inp1.useDelimiter("\\A").next();
+						array1 = b.toLowerCase().split(" ");
+						newmap = CountFrequency(array1);
+					}
+					inp1.close();
+
 					System.out.print(bag.plagerismChecker(map, newmap)+"\t\t");
+
 				}
 				System.out.println();
 			}
+
+
+
 		} catch (Exception e) {
 			System.out.println("empty directory");
 		}
 	}
-	public static  HashMap<String, Integer> Countfrequency(String[] array) {
+	public static  HashMap<String, Integer> CountFrequency(String[] array) {
 		Bagofwords bag = new Bagofwords();
 		for (int i = 0 ; i < array.length; i++) {
 			int value = 1;
-			if (bag.getdictionary().containsKey(array[i])) {
-				value = bag.getdictionary().get(array[i]) + 1;
+			if (bag.getDictionary().containsKey(array[i])) {
+				value = bag.getDictionary().get(array[i]) + 1;
 			}
-			bag.getdictionary().put(array[i], value);
+			bag.getDictionary().put(array[i], value);
 		}
-		return bag.getdictionary();
+		return bag.getDictionary();
 	}
 }
 class Bagofwords {
-	private HashMap<String, Integer> arraydictionary;
+	private  HashMap<String, Integer> dictionary;
+
 	Bagofwords() {
-		arraydictionary = new HashMap<>();
+		dictionary = new HashMap<>();
 	}
-	public HashMap<String, Integer> getdictionary() {
-		return arraydictionary;
+
+	public HashMap<String, Integer> getDictionary() {
+		return dictionary;
 	}
+
 	public long plagerismChecker(HashMap<String, Integer> map, HashMap<String, Integer> newmap) {
 		int num = 0;
 		for (String checkKey : map.keySet()) {
@@ -74,21 +85,25 @@ class Bagofwords {
 				}
 			}
 		}
+
 		int sqr = 0;
-		for (String key1 : map.keySet()) {
-			sqr += Math.pow(map.get(key1), 2);
+		for (String checkKey : map.keySet()) {
+			sqr += Math.pow(map.get(checkKey), 2);
 
 		}
 		int sqr1 = 0;
-		for (String key2 : newmap.keySet()) {
-			sqr1 += Math.pow(newmap.get(key2), 2);
+		for (String checkKey1 : newmap.keySet()) {
+			sqr1 += Math.pow(newmap.get(checkKey1), 2);
 
 		}
 		double den;
 		den = Math.sqrt(sqr) * Math.sqrt(sqr1);
 
-		double answer = (num / den) * 100;
-		answer = Math.round(answer);
-		return (long)(answer);
+		double ans = (num / den) * 100;
+		ans = Math.round(ans);
+		return (long)(ans);
 	}
+
+
+
 }

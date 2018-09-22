959cc7f053894cd71c7e09b2f13157f209b587c4
Abijithhebbar <abijith.hebbar@msitprogram.net>
2018-09-22 12:14:24 +0530

    "check_style_score -> 4 of 14 passed. style score: 0/1 "

--- m25/Assignment-1/Todoist.java
+++ m25/Assignment-1/Todoist.java
@@ -1,14 +1,49 @@
 class Todoist{
+	Task[] task;
+	final int n;
+	String[] result;
+	Todoist() {
+		n = 5;
+		task = new Task[n];
+		result = new String[n];
+	}
+	public void add(Task newtask) {
+		for (int i = 0; i < n; i++) {
+			task[i]= newtask;
+		}
+	}
+	public String toString() {
+		String s = task.toString();
+		return s;
+	}
+
 	int t;
+	// Task[] task =new Task[5];
+	// Todoist() {
+	// 	add();
+	// }
+	// public void add() {
+	// 	for(int i = 0; i < 3; i++) {
+	// 		task[i] = task[i];
+	// 	}
+	// }
 	public String getNextTask(String s) {
 		return s;
 	}
 	public int totalTime4Completion(){
 		return t;
 
-	}
-	// public Task[] getNextTask(String s, int n) {
-	// 	Task[] newtask = new Task();
-	// 	return newtask;
 	// }
+	// public String toString() {
+	// 	String str = "";
+	// 	for (int i = 0; i < 3; i++) {
+	// 		str += task[i];
+	// 	}
+	// 	return str;
+	// }
+	// // public Task[] getNextTask(String s, int n) {
+	// // 	Task[] newtask = new Task();
+	// // 	return newtask;
+	// // }
+}
 }
